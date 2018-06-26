a3f8a3fadfab36e81f4af780d8ee8f337087215c
G. Oliveira Delmiglio <g.oliveira.delmiglio@avanade.com>
2018-06-25 22:35:39 -0300

    First Commit

---
+++ src/app/foto/foto.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'foto',
+  template: `<img src="assets/img/Rostov.jpg" alt="Rostov On Don"`,
+  styles: []
+})
+
+export class FotoComponent { }
